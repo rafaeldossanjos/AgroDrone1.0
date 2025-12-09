4e9866bcfc196bc3b0777cfd91608a93959bf3b1
rafaeldossanjos <rafael.raanjos@gmail.com>
2025-12-08 21:35:23 -0300

    Delete supabase directory

--- supabase/migrations/20251202081801_a04fed4a-c7fc-4ea8-80ed-ba76bc31bb8c.sql
+++
@@ -1,2 +0,0 @@
--- Adicionar campo plot_name na tabela applications
-ALTER TABLE public.applications ADD COLUMN plot_name text;
--- supabase/migrations/20251205083801_deb52349-d564-48ec-ae3b-4af0e4b3c747.sql
+++
@@ -1,4 +0,0 @@
--- Add recommended_dose column to products table for default recommendation
-ALTER TABLE public.products 
-ADD COLUMN recommended_dose DECIMAL(10,4) DEFAULT NULL,
-ADD COLUMN recommended_dose_unit VARCHAR(10) DEFAULT 'L/ha';
